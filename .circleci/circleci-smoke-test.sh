45f49f5c561c87a01bfa319960eaa75507605482
Daniel Burbano <daniel.burbano@gmail.com>
2018-09-13 18:47:15 -0500

    Update

--- .circleci/circleci-smoke-test.sh
+++ .circleci/circleci-smoke-test.sh
@@ -38,7 +38,7 @@ echo "================================================="
 aws devicefarm create-upload --project-arn ${PROJECT_ARN} --name ${IPA_PATH}/${PLATFORM_ENV}/${IPA_FILE}-${PLATFORM_ENV}.ipa --type IOS_APP > ${IPA_OUTPUT_LOG}
 IPA_ARN="`cat ${IPA_OUTPUT_LOG} | grep "arn:"| sed 's/"arn": //'| sed 's/ //g'| sed 's/[",]//g'`"
 IPA_PRESIGNED_URL="`cat ${IPA_OUTPUT_LOG} | grep "url" | sed 's/"url"://g'|sed 's/[",]//g'`"
-curl -T ${IPA_FILE} ${IPA_PRESIGNED_URL}
+curl -T ${IPA_FILE}-${PLATFORM_ENV}.ipa ${IPA_PRESIGNED_URL}
 echo "${IPA_ARN}"
 aws devicefarm get-upload --arn "${IPA_ARN}"
 
